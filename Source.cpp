4cfa10c3a0aa21727dfa58b50b80e5ae62726903
Sergio Miguéns <lonyelon@gmail.com>
2017-09-21 15:58:06 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,49 +1,86 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <SDL.h>
-#include "Neuron.h"
-
-#define e 2.718281828459045
-#define pi 3.141592653589793238462
-
+#include <cmath>
+#include <fstream>
+#include "Network.h"
+ 
 using namespace std;
-
-
-
-int main(int argc, char** args)
+ 
+int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* w = SDL_CreateWindow("NeuralNet", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
-    SDL_Renderer* r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED);
-
-    SDL_Rect r0 = { 100, 100, 100, 100 };
-
-    SDL_Event ev;
-    bool running = true;
-    while (running) 
+    Network n = Network({ 2, 4, 4, 3 });
+    vector<vector<double>> inp
     {
-        while (SDL_PollEvent(&ev) != 0) 
+        { 0, 0 },
+        { 0, 1 },
+        { 1, 0 },
+        { 1, 1 },
+    };
+ 
+    vector<vector<double>> out = 
+    {
+        { 0, 0, 1 },      
+        { 0, 1, 0 },    
+        { 0, 1, 0 },  
+        { 1, 0, 0 },  
+    };
+ 
+ 
+    double c = 0.0000001;
+    while(true)
+    {
+        string s;
+        getline(cin, s);
+ 
+        if (s == "p")
         {
-            if (ev.type == SDL_QUIT) 
-            {
-                running = false;
-            }
-
-            float speed = 1;
+            n.gradient_descent(inp, out, 100000);
+            cout << calc_error(inp, out, n) << endl;
+            cout << "done" << endl;
         }
-
-        SDL_SetRenderDrawColor(r, 21, 21, 21, 255);
-        SDL_RenderClear(r);
-        SDL_SetRenderDrawColor(r, 255, 255, 21, 255);
-        SDL_RenderDrawRect(r, &r0);
-        SDL_RenderPresent(r);
-        SDL_UpdateWindowSurface(w);
-        SDL_Delay(1000/60);
+ 
+        if (s == "input")
+        {
+            vector<double> d;
+            vector<double> r;
+ 
+            for (int i = 0; i < n.get_layer_blueprint()[0]-1; i++)
+            {
+                getline(cin, s);
+                d.push_back(stod(s));
+            }
+ 
+            cout << endl << "Output:" << endl;
+            r = n.get_result(d);
+            //r = n.get_layer_values(1);
+            for(int i = 0; i < r.size(); i++)
+            {
+                cout << "(" << round(r[i]) << ") " << r[i] << endl;
+            }
+        }
+ 
+        if (s == "g")
+        {
+            auto g = n.get_genetics();
+            for (int l = 0; l < g.size(); l++)
+            {
+                for (int n = 0; n < g[l].size(); n++)
+                {
+                    for (int s = 0; s < g[l][n].size(); s++)
+                    {
+                        cout << "[" << l << "," << n << "," << s << "]: " << g[l][n][s] << endl;
+                    }
+                }
+            }
+        }
+ 
+        if (s == "c")
+        {
+            getline(cin, s);
+            c = stod(s);
+        }
     }
-
-    SDL_DestroyWindow(w);
-    SDL_Quit();
-
+ 
     return 0;
 }
